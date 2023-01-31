b1b3b4e2df2a8d863cf31dcf43ae86893f2dd2c5
dragon-shiryu <thigrac@outlook.com>
2023-01-30 23:56:19 -0300

    Exercicio modulo 9

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author ${USER}
+ * @created ${DAY}/${MONTH}/${YEAR} - ${TIME}
+ * @project ${PROJECT_NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
