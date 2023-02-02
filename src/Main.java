5fd998b27b51408cdb4fcb7b74170ea4e8252b06
dragon-shiryu <thigrac@outlook.com>
2023-02-01 22:28:25 -0300

    tarefa_ControledeFluxo

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,14 @@ public class Main {
 
         System.out.printf("\n%s%.1f%s%.1f%s%.1f%s%.1f\n", "Sua notas foram: ", nota.getNotaUm(), " | ", nota.getNotaDois(), " | ", nota.getNotaTres(), " | ", nota.getNotaQuatro());
         System.out.printf("\n%s%.1f\n", "Sua media foi: ", nota.getMediaNotas());
+
+        if (nota.getMediaNotas()>=7) {
+            System.out.println("Voce esta aprovado! Meus parabens.");
+        } else if (nota.getMediaNotas()>=5) {
+            System.out.println("Voce esta de recuperacao! Um pouco mais de esforco sera necessario. \nA nota de corte e 7.0");
+        } else {
+            System.out.println("Voce foi reprovado! Sinto muito. \nA nota de corte e 7.0 para aprovacao e 5.0 para recuperacao.");
+        }
+
     }
 }
